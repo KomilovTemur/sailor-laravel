0de693de19aeaf110349a4e37619a72b0c77f66a
tim_coder <itteamdev74@gmail.com>
2023-03-16 15:57:21 +0500

    Added new route with blade file

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SiteController::class, 'index'])->name('index');
 Route::get('/about', [SiteController::class, 'about'])->name('about');
+Route::get('/portfolio', [SiteController::class, 'portfolio'])->name('portfolio');
