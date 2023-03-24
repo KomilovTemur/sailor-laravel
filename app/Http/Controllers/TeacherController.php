4fa47f179149ecafdddf7749f8ef3e58865687ad
tim_coder <itteamdev74@gmail.com>
2023-03-24 16:26:15 +0500

    teachers controller

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    public function  create() {
+        view("teachers.create");
+    }
+    public function  store() {
+
+    }
+    public function  delete() {
+
+    }
+}
