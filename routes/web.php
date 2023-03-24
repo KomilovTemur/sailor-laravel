e44cf7218ed1023a4ce1787971dc05583c47128a
tim_coder <itteamdev74@gmail.com>
2023-03-24 16:26:53 +0500

    teachers route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SiteController;
+use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [SiteController::class, 'index'])->name('index');
 Route::get('/about', [SiteController::class, 'about'])->name('about');
 Route::get('/portfolio', [SiteController::class, 'portfolio'])->name('portfolio');
+
+Route::get("teacher", [TeacherController::class, "create"])->name("teacher");
+Route::post("teachers", [TeacherController::class, "store"])->name("store");
+
+
