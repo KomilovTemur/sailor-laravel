c3943bf4f694c3ccce918b91569ad9bbd9bc469c
tim_coder <itteamdev74@gmail.com>
2023-03-28 11:08:50 +0500

    Teacher controller

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -3,16 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TeacherController extends Controller
 {
-    public function  create() {
-        view("teachers.create");
+    public function  create()
+    {
+        $all = DB::table('teacher')->get();
+        return view("teachers.create", ['all' => $all]);
     }
-    public function  store() {
-
-    }
-    public function  delete() {
-
+    public function  store(Request $req)
+    {
+        DB::table('teacher')->insert([
+            'name' => $req->name,
+            'address' => $req->address,
+            'science' => $req->science,
+            'email' => $req->email,
+            'phone' => $req->phone,
+        ]);
+        // DB::table('teacher')->insert([
+        //     'name' => $req->name,
+        //     'address' => $req->address,
+        //     'science' => $req->science,
+        //     'email' => $req->email,
+        //     'phone' => $req->phone,
+        // ]);
+        return redirect("/teacher");
     }
 }
