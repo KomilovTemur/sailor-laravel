f6fb62ef141964371625b1fc82c3eb1b458cc419
tim_coder <itteamdev74@gmail.com>
2023-04-01 13:02:47 +0500

    project's blade files

--- resources/views/teachers/create.blade.php
+++ resources/views/teachers/create.blade.php
@@ -1,63 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css">
-</head>
-
-<body>
-
-  <div class="container my-5">
+@extends('layouts/crud')
+@section('content')
     <h2>Create Teacher</h2>
-    <form action="{{ route('store') }}" method="POST">
-      @csrf
-      <input placeholder="name" type="text" class="form-control" name="name" />
-      <input placeholder="address" type="text" class="form-control my-2" name="address" />
-      <input placeholder="science" type="text" class="form-control" name="science" />
-      <input placeholder="email" type="email" class="form-control my-2" name="email" />
-      <input placeholder="phone" type="number" class="form-control" name="phone" />
-      <button class="btn btn-success my-2" type="submit">Submit</button>
+    <form action="{{ route('teacher.store') }}" method="POST">
+        @csrf
+        @method('POST')
+        <input placeholder="name" type="text" class="form-control" name="name"/>
+        <input placeholder="address" type="text" class="form-control my-2" name="address"/>
+        <input placeholder="science" type="text" class="form-control" name="science"/>
+        <input placeholder="email" type="email" class="form-control my-2" name="email"/>
+        <input placeholder="phone" type="number" class="form-control" name="phone"/>
+        <button class="btn btn-success my-2" type="submit">Submit</button>
     </form>
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">name</th>
-          <th scope="col">Address</th>
-          <th scope="col">Science</th>
-          <th scope="col">Email</th>
-          <th scope="col">Phone</th>
-          <th scope="col">Status</th>
-          <th scope="col">actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($all as $one)
-          <tr>
-            <th scope="row">{{ $one->id }}</th>
-            <td>{{ $one->name }}</td>
-            <td>{{ $one->address }}</td>
-            <td>{{ $one->science }}</td>
-            <td>{{ $one->email }}</td>
-            <td>{{ $one->phone }}</td>
-            <td>{{ $one->status }}</td>
-            <td>
-              <a href="/edit/{{ $one->id }}" class="btn btn-success">
-                Edit
-              </a>
-              <a href="/delete/{{ $one->id }}" class="btn btn-danger">
-                Delete
-              </a>
-            </td>
-          </tr>
-        @endforeach
-      </tbody>
-    </table>
-  </div>
-</body>
-
-</html>
+@endsection
--- resources/views/teachers/index.blade.php
+++ resources/views/teachers/index.blade.php
@@ -1,1 +1,39 @@
-<?php
+@extends('layouts/crud')
+@section('content')
+    <div class="d-flex align-items-center justify-content-between py-5">
+        <h2>Teachers</h2>
+        <div class="col-lg-2">
+            <a href="{{ route('teacher.create') }}" class="btn btn-primary">Add Teacher</a>
+        </div>
+    </div>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Address</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Science</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($teachers as $teacher)
+            <tr>
+                <th scope="row">{{ $teacher->id }}</th>
+                <td>{{ $teacher->name }}</td>
+                <td>{{ $teacher->address }}</td>
+                <td>{{ $teacher->email }}</td>
+                <td>{{ $teacher->phone }}</td>
+                <td>{{ $teacher->science }}</td>
+                <td>
+                    <a href="#" class="btn btn-info">View</a>
+                    <a href="#" class="btn btn-warning">Edit</a>
+                    <a href="#" class="btn btn-danger">Delete</a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@endsection
