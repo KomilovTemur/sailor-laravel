9e94eea10d9c958229ffc91a25d396e288e9c26c
tim_coder <itteamdev74@gmail.com>
2023-04-01 13:02:08 +0500

    new TeacherController created by resource contoller

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -1,33 +1,77 @@
 <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+    use App\Models\Teacher;
+    use Illuminate\Http\Request;
 
-class TeacherController extends Controller
-{
-    public function  create()
+    class TeacherController extends Controller
     {
-        $all = DB::table('teacher')->get();
-        return view("teachers.create", ['all' => $all]);
+        /**
+         * Display a listing of the resource.
+         */
+        public function index()
+        {
+            /*$teachers = Teacher::where('science', 'Guliston shaxri')->get();*/
+            /*$teachers = Teacher::inRandomOrder()->get();*/
+            $teachers = Teacher::orderBy('id', 'DESC')->get();
+            //$teachers = Teacher::where('address', 'Guliston shaxri')->where('science', 'history')->get();
+            return view('teachers.index', ["teachers" => $teachers]);
+        }
+
+        /**
+         * Show the form for creating a new resource.
+         */
+        public function create()
+        {
+            return view('teachers.create');
+        }
+
+        /**
+         * Store a newly created resource in storage.
+         */
+        public function store(Request $request)
+        {
+            //$teacher = new Teacher;
+            //$teacher->name = $request->name;
+            //$teacher->address = $request->address;
+            //$teacher->science = $request->science;
+            //$teacher->email = $request->email;
+            //$teacher->phone = $request->phone;
+            //$teacher->save();
+            Teacher::create($request->all());
+            return redirect()->route('teacher.index');
+        }
+
+        /**
+         * Display the specified resource.
+         */
+        public function show(string $id)
+        {
+            //
+        }
+
+        /**
+         * Show the form for editing the specified resource.
+         */
+        public function edit(string $id)
+        {
+            //
+        }
+
+        /**
+         * Update the specified resource in storage.
+         */
+        public function update(Request $request, string $id)
+        {
+            //
+        }
+
+        /**
+         * Remove the specified resource from storage.
+         */
+        public function destroy(string $id)
+        {
+            //
+        }
     }
-    public function  store(Request $req)
-    {
-        DB::table('teacher')->insert([
-            'name' => $req->name,
-            'address' => $req->address,
-            'science' => $req->science,
-            'email' => $req->email,
-            'phone' => $req->phone,
-        ]);
-        // DB::table('teacher')->insert([
-        //     'name' => $req->name,
-        //     'address' => $req->address,
-        //     'science' => $req->science,
-        //     'email' => $req->email,
-        //     'phone' => $req->phone,
-        // ]);
-        return redirect("/teacher");
-    }
-}
