8c4017d2b755cc1534aef06250b466e85cf0be95
tim_coder <itteamdev74@gmail.com>
2023-04-08 10:42:50 +0500

    new migration file

--- database/migrations/2023_04_06_092656_add_image_column_to_teacher_table.php
+++ database/migrations/2023_04_06_092656_add_image_column_to_teacher_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('teacher', function (Blueprint $table) {
-            //
+            $table->string('image')->nullable()->default('default.png');
         });
     }
 
