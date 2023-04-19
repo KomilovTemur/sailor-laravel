dc0bbe053d86ad2f1d96236e45ba4ccca86b9842
Temur Komilov <itteamdev74@gmail.com>
2023-04-19 00:39:37 -0700

    some changes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 })->name('index');
-Route::get('/about', function () { 
+Route::get('/about', function () {
     return view('about');
 })->name('about');
 Route::get('/portfolio', function () {
@@ -48,4 +48,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
