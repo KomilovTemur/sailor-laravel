34517c4a8f708ac691d2212c3a35045f726af57e
Temurbek Komilov <itteamdev74@gmail.com>
2023-06-03 16:27:12 +0500

    some changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,109 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<!DOCTYPE html>
+<html lang="en">
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+
+<!-- auth-register.html  21 Nov 2019 04:05:01 GMT -->
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Otika - Admin Dashboard Template</title>
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="adminAssets/css/app.min.css">
+  <link rel="stylesheet" href="adminAssets/bundles/jquery-selectric/selectric.css">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="adminAssets/css/style.css">
+  <link rel="stylesheet" href="adminAssets/css/components.css">
+  <!-- Custom style CSS -->
+  <link rel="stylesheet" href="adminAssets/css/custom.css">
+  <link rel='shortcut icon' type='image/x-icon' href='adminAssets/img/favicon.ico' />
+</head>
+
+<body>
+  <div class="loader"></div>
+  <div id="app">
+    <section class="section">
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
+            <div class="card card-primary">
+              <div class="card-header">
+                <h4>Register</h4>
+              </div>
+              <div class="card-body">
+                <form action="{{ route('register') }}" method="POST">
+                  @csrf
+                  <div class="row">
+                    <div class="form-group col-6">
+                      <label for="frist_name">Name</label>
+                      <input value="{{ old('name') }}" id="frist_name" type="text" class="form-control"
+                        name="name" autofocus>
+                      @error('name')
+                        {{ $message }}
+                      @enderror
+                      {{-- <x-input-error :messages="$errors->get('name')" class="mt-2" /> --}}
+                    </div>
+                    <div class="form-group col-6">
+                      <label for="email">Email</label>
+                      <input value="{{ old('email') }}" id="email" type="email" class="form-control"
+                        name="email">
+                      @error('email')
+                        {{ $message }}
+                      @enderror
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="form-group col-6">
+                      <label for="password" class="d-block">Password</label>
+                      <input id="password" type="password" class="form-control pwstrength" data-indicator="pwindicator"
+                        name="password">
+                      <div id="pwindicator" class="pwindicator">
+                        <div class="bar"></div>
+                        <div class="label"></div>
+                      </div>
+                      @error('password')
+                        {{ $message }}
+                      @enderror
+                    </div>
+                    <div class="form-group col-6">
+                      <label for="password2" class="d-block">Password Confirmation</label>
+                      <input id="password2" type="password" class="form-control" name="password_confirmation">
+                      @error('password_confirmation')
+                        {{ $message }}
+                      @enderror
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="custom-control custom-checkbox">
+                      <input type="checkbox" name="agree" class="custom-control-input" id="agree">
+                      <label class="custom-control-label" for="agree">I agree with the terms and conditions</label>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block">
+                      Register
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <div class="mb-4 text-muted text-center">
+                Already Registered? <a href="{{ route('login') }}">Login</a>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+      </div>
+    </section>
+  </div>
+  <!-- General JS Scripts -->
+  <script src="adminAssets/js/app.min.js"></script>
+  <!-- JS Libraies -->
+  <script src="adminAssets/bundles/jquery-pwstrength/jquery.pwstrength.min.js"></script>
+  <script src="adminAssets/bundles/jquery-selectric/jquery.selectric.min.js"></script>
+  <!-- Page Specific JS File -->
+  <script src="adminAssets/js/page/auth-register.js"></script>
+  <!-- Template JS File -->
+  <script src="adminAssets/js/scripts.js"></script>
+  <!-- Custom JS File -->
+  <script src="adminAssets/js/custom.js"></script>
+</body>
