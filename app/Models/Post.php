ec342c04ae3ffd9b58b9e144a80ade5f0d9ce391
Temurbek Komilov <itteamdev74@gmail.com>
2023-06-08 21:40:53 +0500

    Category & Post Models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Post extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'body',
+        'category_id',
+        'image',
+        'slug',
+    ];
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
